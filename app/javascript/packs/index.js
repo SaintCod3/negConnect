e029094825e5be90b8d2efcd53cbe5dfc9ce46d7
SaintCod3 <damianmartin95@gmail.com>
2020-10-11 19:29:31 +0100

    Deploy changes

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import $ from "jquery";
 import Popper from "popper.js";
-import "bootstrap/dist/js/bootstrap.bundle.min";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
